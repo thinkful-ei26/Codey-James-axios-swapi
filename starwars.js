4093f48c7c5bcdfbffae50488a0b6e2c6e02cf25
Codey Worley <codeyworley@gmail.com>
2018-11-15 10:34:31 -0700

    added ships/vehicles promises

--- starwars.js
+++ starwars.js
@@ -13,22 +13,41 @@ axios.get(`https://swapi.co/api/people/?search=${searchTerm}`)
     // handle success
     // axios.get()
 
+   const myArray = response.data.results[0];//works
 
-   //console.log(response.data.results[0].name);//works
-   const myArray = response.data.results[0].films;//works
-
-   Promise.all(myArray.map(element => axios.get(element)))
+   Promise.all(myArray.films.map(element => axios.get(element)))
    .then(response=>{
+     console.log('--Movies--');
      for(let i=0; i < response.length; i++){
-        console.log("i= ", i, " space " , response[i].data.title);
+        console.log(response[i].data.title);
      }
+     console.log('\n');
    });
-   
+
+   Promise.all(myArray.starships.map(element => axios.get(element)))
+   .then(response=>{
+    console.log('--Starships--');
+     for(let i=0; i < response.length; i++){
+        console.log(response[i].data.name);
+     }
+     console.log('\n');
+   });
+
+   Promise.all(myArray.vehicles.map(element => axios.get(element)))
+   .then(response=>{
+    console.log('--Vehicles--');
+     for(let i=0; i < response.length; i++){
+        console.log(response[i].data.name);
+     }
+     console.log('\n');
+   });
+
   })
   .catch(function (error) {
     // handle error
     console.log("line 19: ", error);
   });
+
 //   .then(function () {
 //     // always executed
 //   });
